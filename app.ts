d8e9f564ba4647f9edab35d28bcc1f4ad76839ca
Bharat Shah <akashche72@gmail.com>
2022-01-05 17:54:14 +0400

    created app ts to handling all route related and app configuration setting, setting trust proxy if node application is behind the proxy, accept json payload, cookie session so that I will use cookie as seeion for the client, if any route access which is not available then throw an error

---
+++ app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+// While using nginx/apache as proxy set this to true 
+app.set('trust proxy', true); 
+app.use(json());
+app.use(cookieSession({
+    signed: false, 
+    secure: process.env.NODE_ENV !== 'test'
+}));
+
+app.all('*', async(req, res) => {
+    throw new Error('Unable to find the route');
+});
+
+export {app}
